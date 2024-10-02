399a100176a0e395768fda89bcb730355c95fc93
Adams Adeyiga <adamsadeyiga1@gmail.com>
2024-10-02 10:06:53 +0100

    add page

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,12 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   plugins: [react(), ghPages()],
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Explicitly tell Vite where the entry point is
+    }
+  },
   base: '/adamsadeyiga/', // Replace <repository-name> with your repo's name
 });
+
 
