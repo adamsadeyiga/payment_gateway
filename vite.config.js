0cad3945012ebbaf5bab8cf0d6287036b17de5d8
Adams Adeyiga <adamsadeyiga1@gmail.com>
2024-10-02 09:54:51 +0100

    add git page

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: '/adamsadeyiga/', // Replace <repository-name> with your repo's name
+});
+
